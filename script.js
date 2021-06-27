3faa09ca3fc239cdf40e02a8d7d5d1ad6cfc2f69
isaias-silva <zackblackmangold@gmail.com>
2021-06-26 21:17:30 -0300

    metodos de salvamento local ocupam muita memoria sera implementado no futuro com backend

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let x = 0;
 let tablee = window.document.getElementById("tab");
 let b_cad = window.document.getElementById("cadastro");
 b_cad.addEventListener('click', main)
-let a;
-let b;
+
 
 function cadastrar(n, s, d, i) {
 
@@ -63,62 +62,6 @@ function tabela() {
     nova_linha.appendChild(novo_id);
     novo_id.innerHTML = office[x].id_x;
 
-
-
-}
-
-function salvar() {
-    for (let i = 0; i < office.length; i++) {
-        obja[i] = office[i];
-    }
-    saved = JSON.stringify(obja)
-    localStorage.setItem("sv", saved);
-
-
-
-}
-
-
-
-function carregar() {
-
-    a = localStorage.getItem("sv");
-    b = JSON.parse(a);
-
-    for (let i = 0; i < b.length; i++) {
-        let nova_linha = document.createElement("tr");
-        nova_linha.setAttribute("id", `cadastro_${b.id_x}`)
-
-        let novo_nome = document.createElement("td");
-        novo_nome.setAttribute("value", `${b[i].nome}`)
-
-        let novo_sala = document.createElement("td");
-        novo_sala.setAttribute("value", `${b[i].salario}`)
-
-        let nova_data = document.createElement("td");
-        nova_data.setAttribute("value", `${b[i].data_nasc}`)
-
-        let novo_id = document.createElement("td");
-        novo_id.setAttribute("value", `${b[i].id_x}`)
-
-        tablee.appendChild(nova_linha);
-
-        nova_linha.appendChild(novo_nome);
-
-        novo_nome.innerHTML = b[i].nome;
-
-        nova_linha.appendChild(novo_sala);
-        novo_sala.innerHTML = b[i].salario;
-
-        nova_linha.appendChild(nova_data);
-        nova_data.innerHTML = b[i].data_nasc;
-
-        nova_linha.appendChild(novo_id);
-        novo_id.innerHTML = b[i].id_x;
-
-
-
-    }
 
 
 }
